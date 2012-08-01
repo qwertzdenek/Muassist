bf43ba2062809e04fd8a5d61cccfb45983fbb80a
qwertzdenek <jan.zdenek@gmail.com>
2012-08-01 16:09:19 +0200

    Changed structure - test project.

---
+++ tests/src/kiv/janecekz/ma/test/TestMetronome.java
@@ -0,0 +1,11 @@
+package kiv.janecekz.ma.test;
+
+import android.test.AndroidTestCase;
+
+public class TestMetronome extends AndroidTestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+}
