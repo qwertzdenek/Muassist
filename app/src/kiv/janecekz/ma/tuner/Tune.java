3351e5d5ecae53fe55c5611248a74304703ee088
Zdeněk Janeček <jan.zdenek@gmail.com>
2014-03-08 17:51:08 +0100

    Fixed tuner view.

--- app/src/kiv/janecekz/ma/tuner/Tune.java
+++ app/src/kiv/janecekz/ma/tuner/Tune.java
@@ -1,9 +1,11 @@
 package kiv.janecekz.ma.tuner;
 
+import kiv.janecekz.ma.MainActivity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class Tune extends View {
@@ -15,18 +17,16 @@ public class Tune extends View {
 		super(context, attrs);
 
 		setWillNotDraw(false);
-		setVisibility(View.VISIBLE);
 
 		mPaint = new Paint();
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setStrokeWidth(30f);
         mPaint.setColor(0xFFFFFFFF);
-        
-        
 	}
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 		setMeasuredDimension(widthMeasureSpec, 30);
 
 		widthHalf = MeasureSpec.getSize(widthMeasureSpec) / 2;
@@ -35,13 +35,14 @@ public class Tune extends View {
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		Log.d(MainActivity.TAG, "onDraw");
 		
 		mPaint.setARGB(255, 255, (int) Math.ceil(255 - Math.abs(val)*255), (int) Math.ceil(255 - Math.abs(val)*255));
 		canvas.drawLine(widthHalf, 15f, widthHalf + val*(widthHalf - 10), 15f, mPaint);
 	}
 
 	public void setVal(float f) {
-		this.val = f;
-		this.invalidate();
+		Tune.this.val = f;
+		Tune.this.invalidate();
 	}
 }
